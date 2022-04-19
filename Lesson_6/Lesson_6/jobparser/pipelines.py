396964135a85533413cdf548b437f2a8985a4741
dmitrymd <musay@yandex.ru>
2022-04-19 16:31:32 +0400

    update Lesson_6

--- Lesson_6/Lesson_6/jobparser/pipelines.py
+++ Lesson_6/Lesson_6/jobparser/pipelines.py
@@ -7,19 +7,43 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 from pymongo import MongoClient
+from hashlib import md5
+from unicodedata import normalize
+
 
 class JobparserPipeline:
     def __init__(self):
         client = MongoClient('localhost', 27017)
         self.mongobase = client.vacancy_ls6
 
-
     def process_item(self, item, spider):
         item['min_salary'], item['max_salary'], item['currency'] = self.process_salary(item['salary'])
         del item['salary']
+        _id = hash_id(item)
+        item['_id'] = _id
         collection = self.mongobase[spider.name]
         collection.insert_one(item)
         return item
 
     def process_salary(self, salary):
-        return 1, 2, 3
+
+        if len(salary) != 1:
+            currency = salary[-1]
+            if salary[0] == 'до':  # 'от':
+                max_salary = float(normalize('NFKD', salary[1]))
+                min_salary = None
+            elif salary[0] == 'от':
+                min_salary = float(normalize('NFKD', salary[1]))
+                if salary[2] == 'до':
+                    max_salary = float(normalize('NFKD', salary[3]))
+                else:
+                    max_salary = None
+        else:
+            min_salary, max_salary, currency = [None] * 3
+
+        return min_salary, max_salary, currency
+
+    def hash_id(doc):
+        doc_input = str(doc).encode('utf-8')
+
+        return md5(doc_input).hexdigest()
--- Lesson_6/Lesson_6/jobparser/runner.py
+++ Lesson_6/Lesson_6/jobparser/runner.py
@@ -4,12 +4,15 @@ from scrapy.utils.log import configure_logging
 from scrapy.utils.project import get_project_settings
 
 from jobparser.spiders.hhru import HhruSpider
+from jobparser.spiders.superjobru import SuperjobruSpider
 
 if __name__ == '__main__':
     configure_logging()
     settings = get_project_settings()
     runner = CrawlerRunner(settings)
     runner.crawl(HhruSpider)
+    runner2 = CrawlerRunner(settings)
+    runner2.crawl(SuperjobruSpider)
 
     reactor.run()
     
